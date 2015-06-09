334e571abd999ad54606564d9f40240a8884bd66
Evan Lok <evan.lok@opower.com>
2015-06-08 21:10:10 -0700

    Setup test suite and add some tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#base_dir' do
+    let(:video) { build_stubbed(:video) }
+
+    it 'returns base dir for video storage' do
+      expect(video.base_dir).to eq("videos/#{video.id}")
+    end
+  end
+
+  describe '#url' do
+    let(:video) { build_stubbed(:video) }
+
+    before do
+      allow(video).to receive(:base_dir) { 'base' }
+    end
+
+    it 'returns video url' do
+      expect(video.url).to eq("#{ENV['CDN_URL']}/base/#{video.filename}")
+    end
+
+    context 'with version arg' do
+      it 'returns url with version appended' do
+        expect(video.url(720)).to eq("#{ENV['CDN_URL']}/base/#{video.filename.gsub('.mp4', '')}_720.mp4")
+      end
+    end
+  end
 end
