df3b130386abf7bc411b2358788ba51a98bf291e
Giovanni Wong <deargiovanni89@gmail.com>
2015-08-18 01:02:01 -0700

    fixed date formatting

--- app/services/importers/core_logic_location_csv_importer.rb
+++ app/services/importers/core_logic_location_csv_importer.rb
@@ -9,7 +9,9 @@ module Importers
     def import
       CSV.foreach(@file_path).each_with_index do |row, i|
         if row[0] =~ /Period Date:/
-         @cl = CoreLogicLocation.where(period_date: row[0].split(':').last).first_or_initialize
+         date =  row[0].split(':').last.split(/ /).last.split('/')
+         newdate = "#{date[2]}-#{date[0]}-#{date[1]}"
+         @cl = CoreLogicLocation.where(period_date: newdate).first_or_initialize
         end
         next unless row[1] && row[1] != "Tier Name"
         attributes = %w(zip_code tier_name metrics active_list_price_mean active_list_price_median active_listings_dom_mean active_listings_dom_median active_listings_inventory_count sold_inventory_count sold_list_price_mean sold_listings_dom_mean)
