eac9da331aea332f82f44a56acdc0fd011bcfbe9
Evan Lok <elok45@gmail.com>
2016-04-19 10:12:12 -0700

    Fix migration

--- db/migrate/20160418063954_convert_json_columns_to_jsonb.rb
+++ db/migrate/20160418063954_convert_json_columns_to_jsonb.rb
@@ -1,8 +1,8 @@
 class ConvertJsonColumnsToJsonb < ActiveRecord::Migration
   def change
-    change_column :video_contents, :data, :jsonb
-    change_column :scene_contents, :data, :jsonb
-    change_column :video_types, :schema, :jsonb
-    change_column :versions, :object, :jsonb
+    execute 'ALTER TABLE video_contents ALTER COLUMN data SET DATA TYPE jsonb USING data::jsonb;'
+    execute 'ALTER TABLE scene_contents ALTER COLUMN data SET DATA TYPE jsonb USING data::jsonb;'
+    execute 'ALTER TABLE video_types ALTER COLUMN schema SET DATA TYPE jsonb USING schema::jsonb;'
+    execute 'ALTER TABLE versions ALTER COLUMN object SET DATA TYPE jsonb USING object::jsonb;'
   end
 end
