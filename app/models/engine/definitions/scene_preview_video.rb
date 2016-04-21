d554b6c8edd32634d94af37376018093aba76a69
Evan Lok <elok45@gmail.com>
2016-04-21 13:34:10 -0700

    Fix scene previews with json data

--- app/models/engine/definitions/scene_preview_video.rb
+++ app/models/engine/definitions/scene_preview_video.rb
@@ -3,6 +3,8 @@ module Engine
     class ScenePreviewVideo < AbstractDefinition
       attr_reader :scene_vgl, :scene_data
 
+      alias :video_data :scene_data
+
       def initialize(scene_vgl, scene_data = {})
         @scene_vgl = scene_vgl
         @scene_data = Hashie::Mash.new(scene_data.present? ? scene_data : {})
