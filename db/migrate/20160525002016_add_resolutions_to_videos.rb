20809f9d60da97a76c54f444eed2c5d3e98502de
Evan Lok <elok45@gmail.com>
2016-05-26 16:53:09 -0700

    Show medium and high resolutions on video list page

--- db/migrate/20160525002016_add_resolutions_to_videos.rb
+++ db/migrate/20160525002016_add_resolutions_to_videos.rb
@@ -1,5 +1,8 @@
 class AddResolutionsToVideos < ActiveRecord::Migration
   def change
     add_column :videos, :resolutions, :jsonb
+
+    vr = VideoResolutions.new(1280, 720)
+    Video.where(resolutions: nil).update_all resolutions: vr.json
   end
 end
