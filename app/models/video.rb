6008cccb87e7255492f245ddf32fa1d0b206f98d
Evan Lok <elok45@gmail.com>
2016-06-03 12:09:23 -0700

    Remove S3 files when video is deleted

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,14 +5,38 @@ class Video < ActiveRecord::Base
   # Validations
   validates :videoable, :resolutions, presence: true
 
+  # Callbacks
+  after_destroy :remove_s3_files
+
   def base_dir
     "videos/#{id}"
   end
 
-  def url(version=nil)
+  def path(version = nil)
     return nil unless filename
     version_affix = version ? "_#{version}.mp4" : '.mp4'
     version_filename = filename.gsub(/\..+/, version_affix)
-    [ENV['CDN_URL'], base_dir, version_filename].join('/')
+    [base_dir, version_filename].join('/')
+  end
+
+  def url(version=nil)
+    return nil unless filename
+    [ENV['CDN_URL'], path(version)].join('/')
+  end
+
+  private
+
+  def remove_s3_files
+    objects = resolutions.map do |_name, (_width, height)|
+      { key: path(height) }
+    end
+
+    s3 = Aws::S3::Client.new
+    s3.delete_objects(
+      bucket: ENV['S3_BUCKET'],
+      delete: {
+        objects: objects
+      }
+    )
   end
 end
